0a4877d0aa144eca41a7452d1a38583a87a792f8
Slavik Gildeev <vgildeev@r-ideas.org>
2014-10-05 00:23:35 +0300

    remove skrollr parallax

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,4 @@
 $(function() {
-  function adjustWindow(){
-
-    // Setup variables
-    $window = $(window);
-    $slide = $('.homeSlide');
-    isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-    // Init Skrollr
-    if( !isMobile ) {
-      var s = skrollr.init({
-        render: function(data) {
-
-        //Debugging - Log the current scroll position.
-        //console.log(data.curTop);
-        }
-      });
-    }
-
-    // Get window size
-    winH = $window.height();
-
-    // Keep minimum height 550
-    if(winH <= 550) {
-      winH = 550;
-    }
-
-    // Resize our slides
-    $slide.height(winH);
-
-    // Refresh Skrollr after resizing our sections
-    if( !isMobile ) {
-      s.refresh($('.homeSlide'));
-    }
-
-  }
-
-  adjustWindow();
-
   $(".main-nav a").on("click", function() {
     target = $(this).attr('href');
     targetTopPosition = $(target).position().top;
