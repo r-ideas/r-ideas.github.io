c6dfa94df8af54bea9bb6bda97c4e6ed0fc99eb5
Slavik Gildeev <vgildeev@r-ideas.org>
2015-01-27 20:40:10 +0200

    added basic portfolio styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 $(function() {
   $(".main-nav a").on("click", function() {
-    target = $(this).attr('href');
-    targetTopPosition = $(target).position().top;
+    var target = $(this).attr('href');
+    var targetTopPosition = $(target).position().top;
     $("html,body").animate({scrollTop: targetTopPosition}, "1000");
     return false;
   });
